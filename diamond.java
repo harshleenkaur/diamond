24cc67cc91b318134df0ab3836282ead666b2a21
harshleenkaur <47346706+harshleenkaur@users.noreply.github.com>
2019-02-13 15:37:16 +0530

    Add files via upload

---
+++ diamond.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class diamond
+{
+ 
+    public static void main(String[] args)
+	{
+		int n=5;
+		for(int i=0;i<=n;i++)
+		{
+			for(int j=1;j<=n-i;j++)
+			{				
+				System.out.print(" ");
+			}
+			for(int a=1;a<=i;a++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println("");
+		}
+		
+		for(int i=1;i<=n-1;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{				
+				System.out.print(" ");
+			}
+			for(int a=1;a<=n-i;a++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println("");
+		}
+    }
+}
